ea5d21bfd0c11bd483dc3dc9d48ad86a32d58fb7
zeei <chaay3887@gmail.com>
2025-07-14 11:22:53 +0700

    i18n(id): Add new `id.ts` translation

---
+++ src/content/nav/id.ts
@@ -0,0 +1,35 @@
+import { navDictionary } from '../../util/navDictionary';
+
+export default navDictionary({
+	start: 'Mulai',
+	'start.welcome': 'Selamat datang, dunia!',
+	'start.newProject': 'Mulai proyek baru',
+	'start.config': 'Konfigurasi',
+	'start.migrate': 'Migrasi ke Astro',
+
+	guides: 'Panduan dan Resep',
+	'guides.routing': 'Routing dan Navigasi',
+	'guides.ui': 'Membangun Antarmuka (UI)',
+	'guides.content': 'Menambahkan Konten ke Situs',
+	'guides.serverRendering': 'Rendering di Sisi Server',
+	'guides.upgrade': 'Pembaruan',
+	'guides.upgrade.major': 'Panduan Pembaruan Besar',
+	'guides.recipes': 'Resep Cara Melakukan',
+
+	reference: 'Referensi',
+	'reference.runtime': 'API Runtime',
+	'reference.other': 'API Pengembangan Lainnya',
+	'reference.syntax': 'Sintaks Template Astro',
+	'reference.experimental': 'Fitur Eksperimental',
+
+	integrations: 'Integrasi',
+	'integrations.ui': 'Framework UI',
+	'integrations.adapters': 'Adapter',
+	'integrations.other': 'Integrasi Resmi Lainnya',
+
+	thirdParty: 'Layanan Pihak Ketiga',
+	'thirdParty.deployment': 'Panduan Deploy',
+	'thirdParty.cms': 'Sistem Manajemen Konten (CMS)',
+	'thirdParty.backend': 'Layanan Backend',
+	'thirdParty.media': 'Media Hosting & DAM',
+});
